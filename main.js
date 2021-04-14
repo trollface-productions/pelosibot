4bd17740abb02662b2f5255bcb6039dd3c53ee93
trollface-productions <80710383+trollface-productions@users.noreply.github.com>
2021-04-13 17:14:39 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 /*
-  PelosiBot 1.1 - serves a random raunchy image of Nancy Pelosi
+  PelosiBot 1.2 - serves a random raunchy image of Nancy Pelosi
   Now with some other pointless stuff!
+  Note to self, this code needs serious refactoring...
 */
 
 const Discord = require('discord.js');
 const fs = require('fs');
 let STATE = {};
 
-const PELOSIBOT_VERSION = '1.1';
-const PELOSI_ONLY_EXPR = /(^|\s)!pelosi($|\s)/;
+const PELOSIBOT_VERSION = '1.2';
+const PELOSI_ONLY_EXPR = /(^|\s)!pelosi($|\s)/; // if !pelosi appears
+const FUD_ONLY_EXPR = /(^|\s)!fud($|\s)/; // if !fud appears
 const TIME_WINDOW = 60000; // ms
 const RATE_LIMIT = 3;
+const PELOSI_ERROR_MSG = 'too thirsty! {} seconds until you can summon Pelosi again';
+const FUD_ERROR_MSG = 'too bearish! {} seconds until you can shill again';
 const DICE_EXPR = /^!roll (\d+)d(\d+)$/;
 const DICE_SIDES = {'4': true, '6': true, '8': true, '10': true, '12': true, '20': true};
 const MAX_DICE = 20;
 const PELOSIBOT_EXPR = /^!pelosibot (.+)/;
 
 const SECRET = process.env.PELOSI_BOT_SECRET;
-const DATA_FILE_PATH = process.env.PELOSI_BOT_FILE_PATH;
+const DATA_FILE_PATH = process.env.YOUR_PATH_HERE_1;
+const FUD_FILE_PATH = process.env.YOUR_PATH_HERE_2;
 
 // ================================ //
 
@@ -40,15 +45,32 @@ const sendTo = (msg, userId, body) => {
   msg.channel.send(`<@!${userId}> ${body}`);
 };
 
-const doPelosi = (msg, userId, timestamp) => {
+// the big ugly pelosi routine
+const doPelosi = (msg, userId, timestamp, state, items, errorMsg) => {
+  if (!userId) {
+    // theoretically not supposed to happen
+    return;
+  }
+
+  // ignore messages from self
+  if (userId == client.user.id) {
+    return;
+  }
+
+  if (!(userId in state)) {
+    state[userId] = {
+      'times': [],
+      'last': null,
+    };
+  }
+
   // rate limit
-  let times = STATE[userId]['times'];
+  let times = state[userId]['times'];
   if (times.length >= RATE_LIMIT) {
     const diff = timestamp - times[0];
     if (diff < TIME_WINDOW) {
       const secs = Math.ceil((TIME_WINDOW - diff) / 1000);
-      const errorMsg = `too thirsty! ${secs} seconds until you can summon Pelosi again`;
-      sendTo(msg, userId, errorMsg);
+      sendTo(msg, userId, errorMsg.replace('{}', secs));
       return null;
     }
     let n = RATE_LIMIT - times.length + 1;
@@ -59,13 +81,16 @@ const doPelosi = (msg, userId, timestamp) => {
   times.push(timestamp);
 
   // fancy stragety to avoid duplication... once, at least
-  let index = rand(PELOSI_ALL.length);
-  if (PELOSI_ALL[index] === STATE[userId]['last']) {
-    index = (index + 1 + rand(PELOSI_ALL.length - 1)) % PELOSI_ALL.length;
+  let index = rand(items.length);
+  if (items[index] === state[userId]['last']) {
+    index = (index + 1 + rand(items.length - 1)) % items.length;
   }
-  STATE[userId]['last'] = PELOSI_ALL[index];
+  state[userId]['last'] = items[index];
 
-  return index;
+  // send out the Pelosi!
+  if (index !== null) {
+    send(msg, items[index]);
+  }
 };
 
 const rollDice = (msg, userId, count, sides) => {
@@ -114,70 +139,71 @@ const sendUsageInfo = (msg, userId) => {
   let usageMsg = '\n```';
   usageMsg += 'PelosiBot v' + PELOSIBOT_VERSION + '\n';
   usageMsg += '~-~-~-~-~-~-~-~\n';
-  usageMsg += '!pelosi \u2022 sends out a raunchy image\n'
-  usageMsg += '!roll <X>d<Y> \u2022 rolls X dice with Y sides\n'
-  usageMsg += '!pelosibot \u2022 this command\n'
-  if (msg.member.hasPermission('ADMINISTRATOR')) {
-    usageMsg += '\nAdmin commands:\n'
-    usageMsg += '!pelosibot list \u2022 lists current image URLs\n'
-    usageMsg += '!pelosibot add <URL> \u2022 adds an image URL to the list\n'
-    usageMsg += '!pelosibot remove <ID> \u2022 removes an image from the list\n'
+  usageMsg += '!pelosi \u2022 sends out a raunchy image\n';
+  usageMsg += '!fud \u2022 let out your inner shill\n';
+  usageMsg += '!roll <X>d<Y> \u2022 rolls X dice with Y sides\n';
+  usageMsg += '!pelosibot \u2022 this command\n';
+  if (msg.member.hasPermission('BAN_MEMBERS')) { // has moderator permission
+    usageMsg += '\nAdmin commands:\n';
+    usageMsg += '!pelosibot list \u2022 lists current image URLs\n';
+    usageMsg += '!pelosibot add <URL> \u2022 adds an image URL to the list\n';
+    usageMsg += '!pelosibot remove <ID> \u2022 removes an image from the list\n';
   }
   usageMsg += '```';
   sendTo(msg, userId, usageMsg);
 };
 
-const addImageUrl = (msg, userId, url) => {
-  if (PELOSI_ALL.indexOf(url) != -1) {
+const addItem = (msg, userId, item, items) => {
+  if (items.indexOf(item) != -1) {
     sendTo(msg, userId, 'already in the list.');
     return;
   }
 
-  PELOSI_ALL.push(url);
+  items.push(item);
   sendTo(msg, userId, `added image.`);
 
-  writeData(PELOSI_ALL.join('\n'));
+  writeData(DATA_FILE_PATH, items.join('\n'));
 };
 
-const removeImage = (msg, userId, id) => {
-  if (!(id in PELOSI_ALL)) {
+const removeItem = (msg, userId, id, items) => {
+  if (!(id in items)) {
     sendTo(msg, userId, 'please specify a valid ID.');
     return;
   }
 
-  if (id == PELOSI_ALL.length - 1) {
-    PELOSI_ALL.pop();
+  if (id == items.length - 1) {
+    items.pop();
   } else {
-    PELOSI_ALL[id] = PELOSI_ALL.pop();
+    items[id] = items.pop();
   }
 
   sendTo(msg, userId, `removed image ${id}.`);
 
-  writeData(PELOSI_ALL.join('\n'));
+  writeData(DATA_FILE_PATH, items.join('\n'));
 };
 
-const doInfoAndAdmin = (msg, userId, text) => {
+const doInfoAndAdmin = (msg, userId, text, items) => {
   const groups = PELOSIBOT_EXPR.exec(text);
   if (!groups) {
     sendUsageInfo(msg, userId);
     return;
   }
 
-  if (!msg.member.hasPermission('ADMINISTRATOR')) {
-    sendTo(msg, userId, 'only an admin can run this command.');
+  if (!msg.member.hasPermission('BAN_MEMBERS')) {
+    sendTo(msg, userId, 'no permission to run this command.');
     return;
   }
 
   const cmd = groups[1].split(' ');
   if (cmd[0] === 'list') {
-    if (!PELOSI_ALL.length) {
+    if (!items.length) {
       sendTo(msg, userId, 'no image URLs available!');
       return;
     }
 
     let listMsg = '```';
-    for (let i = 0; i < PELOSI_ALL.length; i++) {
-      listMsg += `[${i}] ${PELOSI_ALL[i]}\n`;
+    for (let i = 0; i < items.length; i++) {
+      listMsg += `[${i}] ${items[i]}\n`;
     }
     listMsg += '```';
 
@@ -188,14 +214,14 @@ const doInfoAndAdmin = (msg, userId, text) => {
       return;
     }
 
-    addImageUrl(msg, userId, cmd[1]);
+    addItem(msg, userId, cmd[1], items);
   } else if (cmd[0] === 'remove') {
     if (!cmd[1]) {
       sendTo(msg, userId, 'please specify an image ID to remove.');
       return;
     }
 
-    removeImage(msg, userId, cmd[1]);
+    removeItem(msg, userId, cmd[1], items);
   }
 };
 
@@ -221,32 +247,13 @@ const processMsg = (msg) => {
   const lower = text.toLowerCase();
 
   if (PELOSI_ONLY_EXPR.exec(lower)) {
-    if (!userId) {
-      // theoretically not supposed to happen
-      return;
-    }
-
-    // ignore messages from self
-    if (userId == client.user.id) {
-      return;
-    }
-
-    if (!(userId in STATE)) {
-      STATE[userId] = {
-        'times': [],
-        'last': null,
-      };
-    }
-
-    // send out the Pelosi!
-    const index = doPelosi(msg, userId, timestamp);
-    if (index !== null) {
-      send(msg, PELOSI_ALL[index]);
-    }
+    doPelosi(msg, userId, timestamp, STATE, PELOSI_ALL, PELOSI_ERROR_MSG);
+  } else if (FUD_ONLY_EXPR.exec(lower)) {
+    doPelosi(msg, userId, timestamp, STATE, FUD_ALL, FUD_ERROR_MSG);
   } else if (lower.startsWith('!roll')) {
     doDice(msg, userId, lower);
   } else if (lower.startsWith('!pelosibot')) {
-    doInfoAndAdmin(msg, userId, text);
+    doInfoAndAdmin(msg, userId, text, PELOSI_ALL);
   }
 };
 
@@ -262,22 +269,22 @@ const readFile = (filename, encoding) => {
   }
 };
 
-const readData = () => {
-  const data = readFile(DATA_FILE_PATH);
+const readData = (filename) => {
+  const data = readFile(filename);
   if (!data) {
-    console.error('failed to read data file!');
+    console.error(`failed to read data file: ${filename}`);
     return false;
   }
   // filter out empties
-  return data.split(/\s+/).filter((x) => x);
+  return data.split(/\n/).filter((x) => x);
 };
 
-const writeData = (body, encoding) => {
+const writeData = (filename, body, encoding) => {
   if (!encoding) {
     encoding = 'utf8';
   }
-  fs.writeFile(DATA_FILE_PATH, body, encoding, (err) => {
-    console.error('failed to write out to data file!');
+  fs.writeFile(filename, body, encoding, (err) => {
+    console.error(`failed to write out to data file: ${filename}`);
   });
 };
 
@@ -298,9 +305,16 @@ if (!SECRET) {
   process.exit(1);
 }
 
-let PELOSI_ALL = readData();
+// pelosi list
+let PELOSI_ALL = readData(DATA_FILE_PATH);
 if (!PELOSI_ALL) {
   PELOSI_ALL = [];
+}
+
+// fud list
+let FUD_ALL = readData(FUD_FILE_PATH);
+if (!FUD_ALL) {
+  FUD_ALL = [];
 }
 
 client.login(SECRET);
